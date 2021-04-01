b314f8310a6f5d09b8348a045925a7fadd55c3ae
Humberto Rivero <63797901+danieldamian09@users.noreply.github.com>
2021-03-31 21:20:48 -0300

    Ejercicios 8 y 9

---
+++ TP 1/ejercicio_8.cpp
@@ -0,0 +1,27 @@
+//Nombre:
+//TP N°:
+//EJ N°:
+//Comentarios:
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int importeSinDescuento, importeConDescuento, porcentajeDelTotal, porcentajeDescuento;
+
+    cout << "Por favor ingrese el importe de la venta sin el descuento" << endl;
+    cin >> importeSinDescuento;
+
+    cout << "Por favor ingrese el importe de la venta con el descuento" << endl;
+    cin >> importeConDescuento;
+
+    porcentajeDelTotal = importeConDescuento * 100 / importeSinDescuento;
+
+    porcentajeDescuento = 100 - porcentajeDelTotal;
+
+    cout << "EL porcentaje de la venta aplicado es de: " << porcentajeDescuento;
+
+
+	return 0;
+}
